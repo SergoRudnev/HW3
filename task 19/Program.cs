c4b6da7fc5f7583e5cdca2b0b6c71d7aebe30377
Sergey Rudnev <sergorudnev@gmail.com>
2022-09-28 22:17:11 +0500

    Task19

---
+++ task 19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"{number} - палиндром.");
+  }
+  else Console.WriteLine($"{number} - не палиндром.");
+}
+
+  CheckingNumber(number);
